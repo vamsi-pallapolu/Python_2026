53dabc016d164359d421b74dc610a49e16042b3d
vamsi-pallapolu <vamsikrishnanaidu16@gmail.com>
2026-07-30 09:45:48 -0400

    Add variables basics examples

---
+++ Basics/2_variables.py
@@ -0,0 +1,18 @@
+# same value can be assigned to multiple variables in the same line
+
+a = b = c = 20
+
+print(a, b, c)
+
+
+# Assigning different variables with differnt values in the same line
+x, y, z = "vamsi", 29, "mathworks"
+print(x, y, z)
+
+
+# Importnat notes
+
+"""
+Python variables store references to object not the actual values
+
+"""
