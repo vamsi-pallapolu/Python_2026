4fe39b03aba0a740d321988f68d770a702a0da9a
vamsi-pallapolu <vamsikrishnanaidu16@gmail.com>
2026-07-30 06:17:36 -0400

    Add I/O basics examples

---
+++ Basics/1_io.py
@@ -0,0 +1,26 @@
+# Input - Output
+
+
+name = input("Enter name:")
+# print("Hello,", name, "Welcome!")
+print(f"Hello, {name}")
+
+# s = "vamsi"
+# age = 25
+# city = "NewYork"
+
+# print(s, age, city)
+
+
+# Input 2 numbers
+
+x, y = input("Enter numbers").split()
+print(x, y)
+
+
+# changing input type
+i = int(input("How old are you?"))
+f = float(input("Evaluate 7/2 :"))
+
+print("Age:", i)
+print(f)
