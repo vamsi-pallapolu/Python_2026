c2154256f54952c995c9d063385df119bfbad4ed
vamsi-pallapolu <vamsikrishnanaidu16@gmail.com>
2026-08-01 11:49:19 +0530

    variables done

--- Basics/2_variables.py
+++ Basics/2_variables.py
@@ -16,3 +16,38 @@ print(x, y, z)
 Python variables store references to object not the actual values
 
 """
+
+x = 1
+y = x
+y = y + 1
+print(x)
+print(y)
+
+# deletion of variable
+
+z = 30
+del z
+# print(z)
+
+
+# Lists
+
+v1 = [1, 2, 3]
+v2 = [1, 2 , 3]
+
+print(id(v1))
+print(id(v2))
+
+
+# Practical examples
+# 1 Swapping two variables
+
+a, b = 10, 20
+a, b = b, a
+
+
+# counting characters in a string
+word = "Python"
+length = len(word)
+print("Length of word", length)
+
